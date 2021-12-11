9c60dce467e6a16b153d98a0dc5dd733391d04f1
Arthur Mayer <amayer_ext@christiandior.com>
2021-12-11 21:01:18 +0100

    add CSS shadow onclick nav icons

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -4,34 +4,59 @@ import { useNavigate, useLocation } from "react-router-dom";
 export const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation().pathname;
-  
+
   // Display the switch box color according to route /^\/lapopote\/.*/
   useEffect(() => {
-    let slider = document.querySelector(".slider").style
+    let slider = document.querySelector(".slider").style;
+    let profileIcon = document.getElementById("profile").classList;
+    let homeIcon = document.getElementById("home").classList;
+
     if (location.match(/lapopote/)) {
       slider.background = "var(--popote)";
-      slider.setProperty('--switch', "var(--dark-popote)")
+      slider.setProperty("--switch", "var(--dark-popote)");
     } else if (location.match(/lespopotes/)) {
       slider.background = "var(--popotes)";
-      slider.setProperty('--switch', "var(--dark-popotes)")
+      slider.setProperty("--switch", "var(--dark-popotes)");
     } else {
       slider.background = "grey";
-      slider.setProperty('--switch', "var(--dark)")
+      slider.setProperty("--switch", "var(--dark)");
+      if (location === "/") homeIcon.add("activeIcon");
+      if (location === "/profile") profileIcon.add("activeIcon");
     }
   }, [location]);
-  
+
   const onSwitch = () => {
-    let slider = document.querySelector(".slider").style
+    let slider = document.querySelector(".slider").style;
+    let homeIcon = document.getElementById("home").classList;
+    let profileIcon = document.getElementById("profile").classList;
+
+    profileIcon.remove("activeIcon");
+    homeIcon.remove("activeIcon");
+
     if (location.match(/lapopote/)) {
       navigate("lespopotes");
       slider.background = "var(--popotes)";
-      slider.setProperty('--switch', "var(--dark-popotes)")
-
+      slider.setProperty("--switch", "var(--dark-popotes)");
     } else {
       navigate("lapopote");
       slider.background = "var(--popote)";
-      slider.setProperty('--switch', "var(--dark-popote)")
+      slider.setProperty("--switch", "var(--dark-popote)");
+    }
+  };
 
+  const activeIcon = (id) => {
+    let currentIcon = document.getElementById(id).classList;
+    let homeIcon = document.getElementById("home").classList;
+    let profileIcon = document.getElementById("profile").classList;
+
+    if (id === "home" && currentIcon.length === 1) {
+      navigate("/");
+      currentIcon.add("activeIcon");
+      profileIcon.remove("activeIcon");
+    } else if (id === "profile" && currentIcon.length === 1) {
+      navigate("profile");
+      currentIcon.add("activeIcon");
+      homeIcon.remove("activeIcon");
     }
   };
 
@@ -41,8 +66,8 @@ export const Navbar = () => {
         <input type="checkbox" />
         <span className="slider round" onClick={onSwitch}></span>
       </label>
-      <div className="homeNav" onClick={() => navigate("/")} />
-      <div className="profileNav" onClick={() => navigate("profile")} />
+      <div id="home" className="homeNav" onClick={() => activeIcon("home")} />
+      <div id="profile" className="profileNav" onClick={() => activeIcon("profile")} />
     </div>
   );
 };
