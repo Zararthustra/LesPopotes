ebdd7d2adb57c27f6f9072d1118b7fdd2f09ce1f
Arthur Mayer <amayer_ext@christiandior.com>
2021-12-12 03:20:18 +0100

    add searchbar and filters to la popote

--- client/src/pages/lapopote/lapopote.jsx
+++ client/src/pages/lapopote/lapopote.jsx
@@ -1,41 +1,144 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { Recettes } from "./recettes";
 
 export const Lapopote = () => {
-  const toggleActiveLink = (id) => {
-    let activeLink = document.getElementById(id).classList;
-    let activeLink1 = document.getElementById("1").classList;
-    let activeLink2 = document.getElementById("2").classList;
+  const [apero, setApero] = useState(false);
+  const [entree, setEntree] = useState(false);
+  const [plat, setPlat] = useState(false);
+  const [dessert, setDessert] = useState(false);
+  const [autre, setAutre] = useState(false);
 
-    if (id === "1" && activeLink.length === 1) {
-      activeLink.add("activePopote");
-      activeLink2.remove("activePopote");
-    } else if (id === "2" && activeLink.length === 1) {
-      activeLink.add("activePopote");
-      activeLink1.remove("activePopote");
-    }
-  };
+  // const toggleActiveLink = (id) => {
+  //   let activeLink = document.getElementById(id).classList;
+  //   let activeLink1 = document.getElementById("1").classList;
+  //   let activeLink2 = document.getElementById("2").classList;
+
+  //   if (id === "1" && activeLink.length === 1) {
+  //     activeLink.add("activePopote");
+  //     activeLink2.remove("activePopote");
+  //   } else if (id === "2" && activeLink.length === 1) {
+  //     activeLink.add("activePopote");
+  //     activeLink1.remove("activePopote");
+  //   }
+  // };
   return (
     <div className="headerContainer">
       <h1 className="title lapopote">La Popote</h1>
-      <div className="links">
-        <Link
-          id="1"
-          onClick={() => toggleActiveLink("1")}
-          className="navlink"
-          to="recettes"
-        >
-          Recettes
-        </Link>
-        <Link
-          id="2"
-          onClick={() => toggleActiveLink("2")}
-          className="navlink"
-          to="creation"
-        >
-          Creation
-        </Link>
+      <div className="popoteSubdivision">
+        <div className="search">
+          <div className="searchIcon" />
+          <input type="text" className="searchBar" />
+        </div>
+        <div className="filters">
+          <label className="box">
+            <input
+              type="checkbox"
+              onChange={() => {
+                setApero(!apero);
+              }}
+            />
+            <svg
+              className={`check ${apero ? "check--active" : ""}`}
+              aria-hidden="true"
+              viewBox="0 0 13 10"
+              fill="none"
+            >
+              <path
+                d="M1 4.5L5 9L14 1"
+                strokeWidth="2"
+                stroke={apero ? "#fff" : "none"}
+              />
+            </svg>
+            Apéritifs
+          </label>
+          <label className="box">
+            <input
+              type="checkbox"
+              onChange={() => {
+                setEntree(!entree);
+              }}
+            />
+            <svg
+              className={`check ${entree ? "check--active" : ""}`}
+              aria-hidden="true"
+              viewBox="0 0 13 10"
+              fill="none"
+            >
+              <path
+                d="M1 4.5L5 9L14 1"
+                strokeWidth="2"
+                stroke={entree ? "#fff" : "none"}
+              />
+            </svg>
+            Entrées
+          </label>
+          <label className="box">
+            <input
+              type="checkbox"
+              onChange={() => {
+                setPlat(!plat);
+              }}
+            />
+            <svg
+              className={`check ${plat ? "check--active" : ""}`}
+              aria-hidden="true"
+              viewBox="0 0 13 10"
+              fill="none"
+            >
+              <path
+                d="M1 4.5L5 9L14 1"
+                strokeWidth="2"
+                stroke={plat ? "#fff" : "none"}
+              />
+            </svg>
+            Plats
+          </label>
+          <label className="box">
+            <input
+              type="checkbox"
+              onChange={() => {
+                setDessert(!dessert);
+              }}
+            />
+            <svg
+              className={`check ${dessert ? "check--active" : ""}`}
+              aria-hidden="true"
+              viewBox="0 0 13 10"
+              fill="none"
+            >
+              <path
+                d="M1 4.5L5 9L14 1"
+                strokeWidth="2"
+                stroke={dessert ? "#fff" : "none"}
+              />
+            </svg>
+            Desserts
+          </label>
+          <label className="box">
+            <input
+              type="checkbox"
+              onChange={() => {
+                setAutre(!autre);
+              }}
+            />
+            <svg
+              className={`check ${autre ? "check--active" : ""}`}
+              aria-hidden="true"
+              viewBox="0 0 13 10"
+              fill="none"
+            >
+              <path
+                d="M1 4.5L5 9L14 1"
+                strokeWidth="2"
+                stroke={autre ? "#fff" : "none"}
+              />
+            </svg>
+            Autres
+          </label>
+        </div>
       </div>
-      <Outlet />
+      <Recettes />
     </div>
   );
 };
--- client/src/pages/lapopote/recettes.jsx
+++ client/src/pages/lapopote/recettes.jsx
@@ -1,7 +1,7 @@
 export const Recettes = () => {
     return (
       <div>
-        <h1>Recettes</h1>
+        <h1>Liste des recettes...</h1>
       </div>
     );
 };
