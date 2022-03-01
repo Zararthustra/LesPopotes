c984a173fc066fcee1cfec5fee4e522e93a1669c
Arthur Mayer <arthur.mayer@cap4lab.com>
2022-03-01 15:36:44 +0100

    fix host

--- client/src/assets/utils/host.js
+++ client/src/assets/utils/host.js
@@ -1,3 +1,3 @@
-export const Host = "http://localhost:3001/";
+export const Host = "/";
 //home export const Host = "http://192.168.1.6:3001/";
 //work export const Host = "http://192.168.128.28:3001/";
