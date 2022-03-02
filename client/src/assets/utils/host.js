0d4ecb52b70e695b9bdbeec8d5b27e21363b36f0
Arthur Mayer <arthur.mayer@cap4lab.com>
2022-03-02 16:58:01 +0100

    fix image loader

--- client/src/assets/utils/host.js
+++ client/src/assets/utils/host.js
@@ -1,3 +1,4 @@
 export const Host = "/";
+//export const Host = "http://192.168.1.30:3001/";
 //home export const Host = "http://192.168.1.6:3001/";
 //work export const Host = "http://192.168.128.28:3001/";
