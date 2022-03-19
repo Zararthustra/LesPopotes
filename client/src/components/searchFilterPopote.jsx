72af85a48161cbd4ea3fb1a1aaab661842d25140
Arthur Mayer <arthur.mayer@cap4lab.com>
2022-03-19 12:39:07 +0100

    add boisson filter

--- client/src/components/searchFilterPopote.jsx
+++ client/src/components/searchFilterPopote.jsx
@@ -13,15 +13,18 @@ export const SearchFilterPopote = ({
   const [entree, setEntree] = useState(false);
   const [plat, setPlat] = useState(false);
   const [dessert, setDessert] = useState(false);
+  const [boisson, setBoisson] = useState(false);
   const [autre, setAutre] = useState(false);
   const [showFilters, setShowFilters] = useState(false);
 
-  const allFiltersNotChecked = !apero && !entree && !plat && !dessert && !autre;
-  const onlyAperoChecked = !entree && !plat && !dessert && !autre;
-  const onlyEntreeChecked = !apero && !plat && !dessert && !autre;
-  const onlyPlatChecked = !apero && !entree && !dessert && !autre;
-  const onlyDessertChecked = !apero && !entree && !plat && !autre;
-  const onlyAutreChecked = !apero && !entree && !plat && !dessert;
+  const allFiltersNotChecked =
+    !apero && !entree && !plat && !dessert && !boisson && !autre;
+  const onlyAperoChecked = !entree && !plat && !dessert && !boisson && !autre;
+  const onlyEntreeChecked = !apero && !plat && !dessert && !boisson && !autre;
+  const onlyPlatChecked = !apero && !entree && !dessert && !boisson && !autre;
+  const onlyDessertChecked = !apero && !entree && !plat && !boisson && !autre;
+  const onlyBoissonChecked = !apero && !entree && !plat && !dessert && !autre;
+  const onlyAutreChecked = !apero && !entree && !plat && !dessert && !boisson;
 
   useEffect(() => {
     if (allFiltersNotChecked) setFilter();
@@ -29,6 +32,7 @@ export const SearchFilterPopote = ({
     if (entree) setFilter("entree");
     if (plat) setFilter("plat");
     if (dessert) setFilter("dessert");
+    if (boisson) setFilter("boisson");
     if (autre) setFilter("autre");
   });
 
@@ -155,6 +159,25 @@ export const SearchFilterPopote = ({
               />
             </svg>
             Desserts
+          </label>
+          <label className="box">
+            <input
+              type="checkbox"
+              onChange={() => onlyBoissonChecked && setBoisson(!boisson)}
+            />
+            <svg
+              className={`check ${boisson ? "check--active" : ""}`}
+              aria-hidden="true"
+              viewBox="0 0 13 10"
+              fill="none"
+            >
+              <path
+                d="M1 4.5L5 9L14 1"
+                strokeWidth="2"
+                stroke={boisson ? "#fff" : "none"}
+              />
+            </svg>
+            Boissons
           </label>
           <label className="box">
             <input
