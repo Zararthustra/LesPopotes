ed405bd28c92728989318e2ee90a31c7db625d06
Arthur Mayer <arthur.mayer@cap4lab.com>
2022-03-23 11:59:09 +0100

    update coherent level hierarchy, fix line height for popotesitems

--- client/src/assets/utils/getLevel.js
+++ client/src/assets/utils/getLevel.js
@@ -1,8 +1,8 @@
 export const getLevel = (recipes, notes, popotes, comments) => {
-  const total = recipes * 2 + notes + popotes + comments;
-  if (total < 5) return ["Nouveau", total];
-  if (5 <= total && total <= 10) return ["Curieux", total];
-  if (11 <= total && total <= 20) return ["Amateur", total];
-  if (21 <= total && total < 40) return ["Habitué", total];
-  if (total >= 40) return ["Maître", total];
+  const total = recipes * 1.5 + notes + popotes * 0.5 + comments;
+  if (total < 5) return ["Visiteur", total];
+  if (5 <= total && total <= 10) return ["Plongeur", total];
+  if (11 <= total && total <= 20) return ["Apprenti", total];
+  if (21 <= total && total < 40) return ["Commis", total];
+  if (total >= 40) return ["Chef", total];
 };
